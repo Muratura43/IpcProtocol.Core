ec165b75f7b8ba0e82e5907d2d4d38fe46525da9
Bogdan Radu <bra@heimdalsecurity.com>
2019-09-27 14:54:15 +0300

    Added test console

---
+++ IpcProtocol.TestConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using IpcProtocol.Core;
+
+namespace IpcProtocol.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var protocol = new Protocol<Entity>(8021, 8022);
+
+            // Listen test
+            protocol.Listen((e) =>
+            {
+                Console.WriteLine("Received: " + e.Command);
+            });
+
+            // Send test
+            protocol.Send(new Entity()
+            {
+                Command = "test-send",
+                Payload = null
+            }).SetCallback((e) =>
+            {
+                // Callback test
+                Console.WriteLine("Callbacked: " + e.Command);
+            });
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Entity
+    {
+        public string Command { get; set; }
+        public object Payload { get; set; }
+    }
+}
