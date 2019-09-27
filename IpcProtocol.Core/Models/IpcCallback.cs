82a2a76edd4ad3a32ecc0a8007b937eae43a68b6
Bogdan Radu <bra@heimdalsecurity.com>
2019-09-27 14:41:43 +0300

    Added callback logic

---
+++ IpcProtocol.Core/Models/IpcCallback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IpcProtocol.Core.Models
+{
+    public class IpcCallback<T> where T : new()
+    {
+        public Action<T> CallbackAction { get; private set; }
+        public Guid Id { get; private set; }
+
+        private Protocol<T> _protocol;
+
+        internal IpcCallback(Protocol<T> protocol, Guid id)
+        {
+            _protocol = protocol;
+            Id = id;
+        }
+
+        public void SetCallback(Action<T> callbackAction)
+        {
+            CallbackAction = callbackAction;
+            _protocol.AddCallback(this);
+        }
+    }
+}
--- IpcProtocol.Core/Protocol.cs
+++ IpcProtocol.Core/Protocol.cs
@@ -1,26 +1,36 @@
 ﻿using IpcProtocol.Core.Models;
 using Newtonsoft.Json;
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace IpcProtocol.Core
 {
     public class Protocol<T> where T : new()
     {
         private bool _isListening = false;
+        private int _clientPort;
+        private int _serverPort;
 
         private readonly IpcClient<T> _client;
         private readonly IpcServer _server;
 
-        private Action<IpcEntity<T>> _onMessageReceivedAction;
+        private Action<T> _onMessageReceivedAction;
+
+        private Dictionary<Guid, Action<T>> _callbacks;
+        private volatile object _dictionaryLock = new object();
 
         public Protocol(int clientPort, int serverPort)
         {
+            _clientPort = clientPort;
+            _serverPort = serverPort;
+
             _client = new IpcClient<T>(clientPort);
             _server = new IpcServer(serverPort);
+
+            _callbacks = new Dictionary<Guid, Action<T>>();
         }
 
-        public void Listen(Action<IpcEntity<T>> onMessageReceived)
+        public void Listen(Action<T> onMessageReceived)
         {
             _onMessageReceivedAction += onMessageReceived;
             _server.OnDataReceived += Server_OnDataReceived;
@@ -36,18 +46,41 @@ namespace IpcProtocol.Core
             }
         }
 
-        public Task SendAsync(IpcEntity<T> data)
+        public IpcCallback<T> Send(T data)
         {
-            return Task.Run(() =>
+            var request = new IpcEntity<T>(data, Guid.NewGuid(), _clientPort);
+
+            _client.Send(request);
+            return new IpcCallback<T>(this, request.Header.CallbackId);
+        }
+
+        internal void AddCallback(IpcCallback<T> callback)
+        {
+            lock (_dictionaryLock)
             {
-                _client.Send(data);
-            });
+                _callbacks.TryAdd(callback.Id, callback.CallbackAction);
+            }
         }
 
         private void Server_OnDataReceived(object sender, IpcEventArgs e)
         {
             var entity = JsonConvert.DeserializeObject<IpcEntity<T>>(e.JsonData);
-            _onMessageReceivedAction?.Invoke(entity);
+
+            // See if a callback is assigned to this request and call that
+            if (_callbacks.TryGetValue(entity.Header.CallbackId, out Action<T> cb))
+            {
+                cb?.Invoke(entity.Entity);
+
+                lock (_dictionaryLock)
+                {
+                    _callbacks.Remove(entity.Header.CallbackId);
+                }
+            }
+            else
+            {
+                // If no specific callback exists, call the generic action
+                _onMessageReceivedAction?.Invoke(entity.Entity);
+            }
         }
     }
 }
